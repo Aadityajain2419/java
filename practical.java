7dd99dac255ffd75ae9a78d96fbde8dfd696c3c8
Aadityajain2419 <2503031590493@paruluniversity.ac.in>
2026-06-29 10:52:42 +0530

    Refactor practical.java to Practical1 with corrections

--- practical.java
+++ practical.java
@@ -1,40 +1,35 @@
-public class practical 1 {
-    public static void main(string[] args) {
-        // Hello world
-        system.out.println("Hello world!");
-        system.out.println() ;
+public class Practical1 { 
+    public static void main(String[] args) { 
+        // Hello world 
+        System.out.println("Hello world!"); 
+        System.out.println(); 
 
         //variables and data types 
-        int age = 20;
-        double salary = 35000.75;
-        char grade = 'A';
-        boolean isStudent = true;
-        String name = "Rahul";
+        int age = 20; 
+        double salary = 35000.75; 
+        char grade = 'A'; 
+        boolean isStudent = true; 
+        String name = "Rahul"; 
+        
+        System.out.println("Name:" + name); 
+        System.out.println("Age:" + age); 
+        System.out.println("Salary:" + salary); 
+        System.out.println("Grade:" + grade); 
+        System.out.println(); 
 
-        system.out.println("Name:"+ name);
-        system.out.println("Age:"+age);
-        system.out.println("Salary:"+ salary);
-        system.out.println("Grade:"+ grade);
-        system.out.println();
+        //Implicit Type Casting(automatic) 
+        int number = 50; 
+        double result = number; 
+        System.out.println("implicit type casting"); 
+        System.out.println("integer value:" + number); 
+        System.out.println("converted to double:" + result); 
+        System.out.println(); 
 
-        //Implicit Type Casting(automatic)
-        int number = 50;
-        double result = number;
-
-        system.out.println(x:"implicit type casting");
-        system.out.println("integer value:"+number);
-        system.out.println("converted to double":+result);
-
-        system.out.println();
-
-        //Explicit type casting(manual)
-        double marks = 89.75;
-        int totalMarks = (int) marks;
-
-   
-             System.out.println("Explicit Type casting");
-                  System.out.println("Double value:"+marks);
-                       System.out.println("converted to integer" : +totalMarks);
-
-    }
+        //Explicit type casting(manual) 
+        double marks = 89.75; 
+        int totalMarks = (int) marks; 
+        System.out.println("Explicit Type casting"); 
+        System.out.println("Double value:" + marks); 
+        System.out.println("converted to integer:" + totalMarks); 
+    } 
 }
